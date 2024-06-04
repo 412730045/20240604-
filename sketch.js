112962a1ac12927d1d38704cc8b95de3b5f60d77
412730045 <150640376+412730045@users.noreply.github.com>
2024-06-04 10:40:13 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ function drawSkeleton() {
      push()
       textSize(40)
       scale(-1,1)
-      text("41273045,林昱睿",partA.x-width,partA.y-100)
+      text("412730045,林昱睿",partA.x-width,partA.y-100)
      pop()
         if (partA.score > 0.1) {
       image(carImg, partC.x, partC.y, 50, 50);
